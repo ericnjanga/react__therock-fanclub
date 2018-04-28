d006d1de3fb4317845ca6e46d271fbfd1d11dff0
Eric Njanga <njanga.webdev@gmail.com>
2018-04-28 10:15:12 -0400

    Error fixing

--- src/components__view/ViewMessageBoard/ViewMessageBoard.js
+++ src/components__view/ViewMessageBoard/ViewMessageBoard.js
@@ -7,11 +7,12 @@ import faPencil from '@fortawesome/fontawesome-free-solid/faPencilAlt';
 import { Button, Container, Row, Col } from 'reactstrap';
 import Figure from './../../components__widget/Figure/Figure.js';
 import DBPost from '../../utilities/DBPost.class.js'; 
+import ViewApp from './../ViewApp.js';
 import './ViewMessageBoard.css'; 
 import img1 from './../../images/therock-1.jpeg'; 
  
 
-class ViewMessageBoard extends React.Component {
+class ViewMessageBoard extends ViewApp {
   constructor(props) {
     super(props);
     this.state = {
@@ -32,21 +33,22 @@ class ViewMessageBoard extends React.Component {
      * Fetch all elements of listA and for each element of listA:
      * -> find the corresponding element in list B, join it to A and save it into a final list
      */
-    DBPost.getNode().on('value', (snapshot) => {
-      //Get data (iterable object)
+    DBPost.getNode().on('value', (snapshot) => { 
       const nodeVal = snapshot.val(); 
-      const postMap = new Map(Object.entries(nodeVal));
-      //push values in a regular array
       let itemsList = [];
-      postMap.forEach((value, key)=>{
-        let post = Object.assign({}, value);
-        post.id = key;
-        itemsList.push(post);
-      });
-      //save reverse array in state (most recent posts first)
-      this.setState({
-        itemsList: itemsList.reverse()
-      }); 
+      if(nodeVal){ //Avoid error if there is no DB objects
+        const postMap = new Map(Object.entries(nodeVal));
+        postMap.forEach((value, key)=>{
+          let post = Object.assign({}, value);
+          post.id = key;
+          //push values in a regular array 
+          itemsList.push(post);
+          itemsList = itemsList.reverse();
+        });
+      }else{
+        //save reverse array in state (most recent posts first)
+        this.setState({ itemsList });
+      }  
     });//[end] within nodeRef_A  
   }//[edn] componentDidMount
  
